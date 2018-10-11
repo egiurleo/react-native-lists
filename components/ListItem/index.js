56db9c5cd39cd7b31a02cc36d93875890e53fbef
Emily Giurleo <e.m.giurleo@gmail.com>
2018-10-11 09:57:38 -0400

    add notion of children to unordered list

---
+++ components/ListItem/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { View, Text } from "react-native";
+
+class ListItem extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello world</Text>
+      </View>
+    );
+  }
+}
+
+export default ListItem;
--- components/UnorderedList/index.js
+++ components/UnorderedList/index.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { View, Text } from "react-native";
+import ListItem from "components/ListItem";
 import styles from "./styles";
 
 class UnorderedList extends Component {
-  static propTypes = {};
+  static propTypes = {
+    children: PropTypes.array
+  };
+
+  static defaultProps = {
+    children: []
+  };
 
   render() {
     return (
       <View>
-        <Text>Hello</Text>
+        {this.props.children.map(child => {
+          if (child.type == ListItem) {
+            return (
+              <View>
+                {"-"}
+                {child}
+              </View>
+            );
+          }
+
+          return child;
+        })}
       </View>
     );
   }

c8d5efd26334cf3ad174bb58057e5c979917dd56
Emily Giurleo <e.m.giurleo@gmail.com>
2018-10-11 09:36:53 -0400

    changing to unordered list component

---
+++ components/UnorderedList/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+class UnorderedList extends Component {
+  static propTypes = {};
+
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+export default UnorderedList;
--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { View, Text } from "react-native";
-
-class reCAPTCHAv3 extends Component {
-  static propTypes = {};
-
-  render() {
-    return (
-      <View>
-        <Text>Hello</Text>
-      </View>
-    );
-  }
-}
-
-export default reCAPTCHAv3;
+export { default as UnorderedList } from "components/UnorderedList";
